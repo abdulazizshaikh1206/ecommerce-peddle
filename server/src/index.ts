786764090a2d5df9c4dd8c35fb1c9afaf4614eea
Abdulaziz Shaikh <abdulaziz.shaikh1206@gmail.com>
2024-05-30 19:54:07 +0530

    Initialize express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
